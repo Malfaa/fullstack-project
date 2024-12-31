cad7c99e91cbe5deafc6729ccaea5ed19e9ae232
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2024-12-31 16:18:12 +0000

    feat: dependências e server-side

--- src/app/server.ts
+++ src/app/server.ts
@@ -3,3 +3,11 @@ import express from "express";
 const app = express();
 const host = "127.0.0.0";
 const port = 3000;
+
+app.get("/anotacoes", (req, res) => {
+    res.send("hello world");
+})
+
+app.listen(port, host, () => {
+    console.log(`Servidor Ativo:${host}:${port}`);
+});
